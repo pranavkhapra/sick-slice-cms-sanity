b4e35daaaff599cf3042cca71fa5a78c84e9c71d
pranavkhapra <pranavkhapra23@gmail.com>
2021-03-05 15:08:36 +0530

    completed all the schema and also created my custom cms input(price)

---
+++ components/PriceInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
+
+function createPatchFrom(value) {
+  return PatchEvent.from(value === '' ? unset() : set(Number(value)));
+}
+const formatMoney = Intl.NumberFormat('en-IN', {
+  style: 'currency',
+  currency: 'USD',
+}).format;
+
+function PriceInput(props) {
+  return (
+    <>
+      <h2>
+        {props.type.title} - {formatMoney(props.value / 100)}
+      </h2>
+      <p>{props.type.description}</p>
+      <input
+        type={props.type.type.name}
+        value={props.value}
+        onChange={(event) =>
+          props.onChange(createPatchFrom(event.target.value))
+        }
+      />
+    </>
+  );
+}
+
+export default PriceInput;
