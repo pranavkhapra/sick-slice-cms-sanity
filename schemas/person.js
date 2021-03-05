6055f07cf37f618bc9fd64875044ee055ec7aebc
pranavkhapra <pranavkhapra23@gmail.com>
2021-03-05 14:13:37 +0530

    created the schemas for the pizza person(slicemaster) and the toppings on the schema and created a data relation between the toppings of the pizza and the pizza

---
+++ schemas/person.js
@@ -0,0 +1,39 @@
+import { BsFillPersonFill as icon } from 'react-icons/bs';
+
+export default {
+  name: 'person',
+  title: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      description: 'Name of the Person',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us a bit about the Person',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
--- schemas/pizza.js
+++ schemas/pizza.js
@@ -36,5 +36,31 @@ export default {
       description: 'Price of the pizza in cents',
       validation: (Rule) => Rule.min(1000).max(50000), // 10 dollar ot 50 dollar pizza
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'topinngs.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      const tops = Object.values(toppings).filter(
+        (topping) => topping !== undefined
+      );
+      return {
+        title,
+        media,
+        subtitle: tops.join(','),
+      };
+    },
+  },
 };
